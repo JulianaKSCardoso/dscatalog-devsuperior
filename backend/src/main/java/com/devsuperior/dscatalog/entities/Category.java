48a88a14a5304230f2ad8a488bdd375a397a617f
Juliana <cardosojuliana@live.com>
2022-01-25 08:11:03 -0300

    Conf. H2 database

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,11 +1,24 @@
 package com.devsuperior.dscatalog.entities;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_category")
 public class Category implements Serializable {
+	
+	@Serial
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 
