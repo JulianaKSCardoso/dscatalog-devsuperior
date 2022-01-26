01d46ee55303a4e99c3db6983e73bf358fcb9903
Juliana <cardosojuliana@live.com>
2022-01-26 10:13:49 -0300

    Delete category

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/exceptions/DatabaseException.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dscatalog.services.exceptions;
+
+public class DatabaseException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
